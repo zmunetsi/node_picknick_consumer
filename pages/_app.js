0134cebdc7fdea70e37f37b8d5ae56d860e3e3b6
Munetsi Zunguzira <dev@jetweb.onmicrosoft.com>
2022-09-16 10:52:34 +0200

    initial commit nextjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
-import '../styles/globals.css'
+// import prime react
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
